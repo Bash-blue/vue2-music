89ec47f83abc3a61b8f64b24d86816ed291b399b
2432682768 <2432682768@qq.com>
2024-11-19 19:36:48 +0800

    搜索功能初步

--- src/views/HomeSearch.vue
+++ src/views/HomeSearch.vue
@@ -1,13 +1,75 @@
 <template>
-  <div>HomeSearch</div>
+  <div>
+    <p>请输入你要搜索的歌曲或歌手</p>
+    <input type="text" v-model="question" />
+
+    <section v-if="result">
+      <ul>
+        <li
+          v-for="item in allMatch"
+          @click="fetchDetails(item.keyword)"
+          :key="item.id"
+        >
+          {{ item.keyword }}
+        </li>
+      </ul>
+    </section>
+
+    <section v-if="songesult">
+      <ul>
+        <NewsonglistCard
+          v-for="item in songMatch"
+          :key="item.id"
+          :item="item"
+        ></NewsonglistCard>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
+import NewsonglistCard from "@/components/NewsonglistCard.vue";
 
-}
+export default {
+  components: {
+    NewsonglistCard,
+  },
+  data() {
+    return {
+      question: "",
+      result: false,
+      allMatch: [],
+      songesult: false,
+      songMatch: [],
+      NewsonglistCard: [],
+    };
+  },
+  watch: {
+    question: function (n) {
+      this.result = true;
+      this.axios
+        .get(
+          `https://apis.netstart.cn/music/search/suggest?keywords="${n}"&type=mobile`
+        )
+        .then((response) => {
+          this.allMatch = response.data.result.allMatch;
+        });
+    },
+  },
+  methods: {
+    fetchDetails(keyword) {
+      this.result = false;
+      this.songesult = true;
+      this.axios
+        .get(`https://apis.netstart.cn/music/search?keywords="${keyword}`)
+        .then((response) => {
+          this.songMatch = response.data.result.songs;
+        });
+    },
+  },
+};
 </script>
 
 <style>
-
+/* Add your styles here */
 </style>
